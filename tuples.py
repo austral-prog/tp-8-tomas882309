7e6b4bf88059c4168a4286ad6703258ff5241894
Tomas Scherer <141924902+tomas882309@users.noreply.github.com>
2024-05-29 23:19:55 -0300

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,6 +1,7 @@
 """Functions to help Azara and Rui locate pirate treasure."""
 
 
+
 def get_coordinate(record):
     """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
 
